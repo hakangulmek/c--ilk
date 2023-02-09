002f149aa5c868dc5e0801cfec1a5386c5933646
hakangulmek <124889788+hakangulmek@users.noreply.github.com>
2023-02-09 15:54:17 +0300

    c# değişkenler

---
+++ ilk/ilk/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string str1 = String.Empty;
+str1 = "Hakan Gülmek";
+String ad = "Hakan";
+string soyad = "Gülmek";
+string tamIsım = ad + "" + soyad;
+
+int integer1 = 6;
+int integer2 = 9;
+int integer3 = integer1 + integer2 ;
+
+bool bool1 = 10 > 2;
+
+string str20 = "20";
+int int20 = 20;
+string yenideger = str20 + int20.ToString();
+Console.WriteLine(yenideger);
+
+int int21 = int20 + Convert .ToInt32(str20);
+Console .WriteLine(int21);
